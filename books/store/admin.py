75150a4af962786219ed6e69e44fa4ffa5838b4d
Bugsy <Toxicbugsy@yandex.com>
2022-04-12 19:42:42 +0300

    add unittest with search and filter, update requirements.txt

--- books/store/admin.py
+++ books/store/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from store.models import Book
 
-admin.site.register(Book)
+# admin.site.register(Book)
+
+
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    pass
--- books/store/tests/test_serializers.py
+++ books/store/tests/test_serializers.py
@@ -6,20 +6,22 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Test book 1', price=25)
-        book_2 = Book.objects.create(name='Test book 2', price=55)
+        book_1 = Book.objects.create(name='Test book 1', price=25, author_name='')
+        book_2 = Book.objects.create(name='Test book 2', price=55, author_name='')
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
-                'price': '25.00'
+                'price': '25.00',
+                'author_name': ''
 
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
-                'price': '55.00'
+                'price': '55.00',
+                'author_name': ''
 
             }
         ]
