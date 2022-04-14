995cee5a2821852fab4f10c032ff688d63b30b2a
Bugsy <Toxicbugsy@yandex.com>
2022-04-14 14:31:24 +0300

    create tests for CRUD without Del

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,6 +11,7 @@ from store.serializers import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
         self.book_2 = Book.objects.create(name='Test book 2', price=55, author_name='Author 5')
         self.book_3 = Book.objects.create(name='Test book Author 1', price=55, author_name='Author 2')
@@ -33,3 +37,49 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BooksSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+
+        data = {
+            "name": "New Code in Python 3",
+            "price": 300,
+            "author_name": "gr"
+        }
+
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+        # Надо обратиться к создаваемому id (book_4) и сравнить созданный результат (data) c ожидаемым (expected_data)
+        # print(Book.objects.name)
+        # print(Book.name())
+        #
+        # затестить: self.book_1.id
+        # в test_update reverse посмотреть - book-detail
+
+        # expected_data = {
+        #     "name": "New Code in Python 3",
+        #     "price": 300,
+        #     "author_name": "gr"
+        # }
+        # self.assertEqual(expected_data, data)
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        # Рефрешим объект из БД чтобы значение прайс подтянулось из data = {"price": 575,}
+        self.book_1.refresh_from_db()
+        self.assertEqual(575, self.book_1.price)
